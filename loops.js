c85e945721713556580f3b1b9d56b2a7778dd943
Cristina Lugo <cmlugoce@syr.edu>
2018-05-09 20:44:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,9 @@
 function forLoop(array) {
   for (var i = 1; i < 25; i++) {
-    if (i===1)
+    if (i===1) {
     array.push("I am 1 strange loop")
-    else
+    }
+    else {
+      array.push("I am ${1} stange loops")
+    }
 }
